6694a0daf5e7db651876e8e6513256370fe5dc16
Choudhary Rahul, SUB-SDC-NL-DOS-ECD <Rahul.Choudhary@swisscom.com>
2022-01-25 15:32:17 +0100

    app in progress

---
+++ api/src/main/java/com/testing/automated/AutomatedApplication.java
@@ -0,0 +1,13 @@
+package com.testing.automated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/testing/automated/AutomatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.automated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
